553f7b2bf36fcd7941f5daae4fbd8636c523f176
Mohammad SN <shahnazi2002@gmail.com>
2021-11-06 15:37:12 +0330

    Add files via upload

---
+++ function.py
@@ -0,0 +1,10 @@
+def isFibonacci(n) :
+    s = []
+    a, b = 0, 1
+    while a <= n :
+        s = [a]
+        a, b = b, a + b
+    if n in s :
+        return True
+    else :
+        return False
