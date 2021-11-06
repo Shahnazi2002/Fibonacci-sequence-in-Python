c5f98ecf8cf56e6bab45f6506f56e7c43f6cf1dc
Mohammad SN <shahnazi2002@gmail.com>
2021-11-06 15:44:32 +0330

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,12 @@
+def isFibonacci(n) :
+    s = []
+    a, b = 0, 1
+    while a <= n :
+        s = [a]
+        a, b = b, a + b
+    if n in s :
+        return True
+    else :
+        return False
+      
+print(isFibonacci(8))
