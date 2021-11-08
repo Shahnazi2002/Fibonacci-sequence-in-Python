a2b00664f3ea2439b45ba988bb201eec90a8546c
Mohammad SN <shahnazi2002@gmail.com>
2021-11-08 08:27:02 +0330

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,12 +1,13 @@
-def isFibonacci(n) :
+def isFibonacci(n):
     s = []
     a, b = 0, 1
-    while a <= n :
+    while a <= n:
         s = [a]
         a, b = b, a + b
-    if n in s :
+    if n in s:
         return True
-    else :
+    else:
         return False
-      
+
+
 print(isFibonacci(8))
