5b67c2610f0cca247cf9c619c49df7378e749681
ObaidaHY <obaidah@mail.tau.ac.il>
2022-06-18 20:48:12 +0300

    changing the implementation of is_Fibonacci function

--- Fibonacci-number-detector-function/function.py
+++ Fibonacci-number-detector-function/function.py
@@ -1,10 +1,11 @@
+import math
+
+# A utility function that returns true if x is perfect square
+def isPerfectSquare(x):
+    s = int(math.sqrt(x))
+    return s*s == x
+
+# Returns true if n is a Fibonacci Number, else false
 def isFibonacci(n):
-    s = []
-    a, b = 0, 1
-    while a <= n:
-        s = [a]
-        a, b = b, a + b
-    if n in s:
-        return True
-    else:
-        return False
+    # n is Fibonacci iff (5*n*n + 4 or 5*n*n - 4) is a perfect square
+    return isPerfectSquare(5*n*n + 4) or isPerfectSquare(5*n*n - 4)
